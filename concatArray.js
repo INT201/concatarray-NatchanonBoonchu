0cbfd9dce1f422f466854d778b9978e7be1579d8
Natchanon Boonchu <natchanon.boonchu@mail.kmutt.ac.th>
2022-09-06 20:44:26 +0700

    add if

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,7 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  console.log("Hello")
+  if((array1 == null || undefined) &&(array2 == null || undefined) )
+  return "undefined";
 }
 module.exports = concatArray
